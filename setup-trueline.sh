ba2d7b6972e17fb7d8116441f264d2958f80b0ab
dlguswo333 <dlguswo333@skku.edu>
2021-08-27 13:56:44 +0900

    Add config shell script

---
+++ setup-trueline.sh
@@ -0,0 +1,43 @@
+# This script will install trueline on bash.
+# Visit https://github.com/petobens/trueline for more information.
+
+# If trueline is already configured in .bashrc, print a warning and exit.
+grep_result=$(grep trueline ~/.bashrc | wc -l)
+if [ "$grep_result" -ne "0" ]; then
+    echo "Looks like trueline is already configured. Take a look and try again."
+    exit 1
+fi
+
+# Get trueline source.
+if [ ! -f "./trueline.sh" ]; then
+wget https://raw.githubusercontent.com/petobens/trueline/master/trueline.sh -P ./
+
+# Export configurations before sourcing trueline.
+echo "
+declare -A TRUELINE_COLORS=(
+    ['deep_green']='101;160;99'
+)
+
+declare -a TRUELINE_SEGMENTS=(
+    'user,white,special_grey,normal'
+    'working_dir,white,deep_green,normal'
+    'git,black,white,normal'
+    'bg_jobs,black,white,bold'
+)
+
+declare -A TRUELINE_SYMBOLS=(
+    ['bg_jobs']='○'
+    ['working_dir_folder']='…'
+    ['working_dir_home']='~'
+    ['git_github']=''
+    ['git_modified']='*'
+    ['git_ahead']='↑'
+    ['git_behind']='↓'
+)
+" >> ~/.bashrc
+
+# Source trueline.
+echo "
+source $PWD/trueline.sh
+" >> ~/.bashrc
+
